7520d72a8d86a9fd67bbc37fa54eb95fe111d9ca
Alex Hawthorn <alexhawthorn@me.com>
2022-11-10 18:34:38 -0800

    Added timestamp
    Via Luxon

--- routes/sms.js
+++ routes/sms.js
@@ -3,31 +3,28 @@ var router = express.Router();
 var MessagingResponse = require('twilio').twiml.MessagingResponse;
 var Message;
 var osc = require('node-osc');
+const { DateTime } = require("luxon");
 
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   console.log("Request:");
-  console.log(req);
+  console.log(req.header);
   console.log("Request Body: ");
   console.log(req.body);
+  var time = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
   Message = req.body.Body;
   const twiml = new MessagingResponse();
 
-  twiml.message('Your message is now en route to Mars.');
+  twiml.message('Message received at ' + time + '.  Your message is now en route to Mars.');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 
   var oscClient = new osc.Client('127.0.0.1', 3333);
   oscClient.send('/sms', req.body.Body);
-  // oscClient.close();
 });
 
-// var oscPort = new osc.WebSocketPort({
-//   url: "ws://localhost:5678", // URL to your Web Socket server.
-//   metadata: true
-// });
 
 router.get('/', function(req, res, next) {
 
@@ -35,17 +32,5 @@ router.get('/', function(req, res, next) {
 
 });
 
-// oscPort.open();
 
 module.exports = router;
-
-// oscPort.on("ready", function () {
-//   oscPort.send({
-//       address: "/msg",
-//       args: [
-//           {
-//               value: Message
-//           }
-//       ]
-//   });
-// });
